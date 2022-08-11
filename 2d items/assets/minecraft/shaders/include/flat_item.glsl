f8240181f1905801f231936cd695603692e3fd12
DartCat25 <82237282+DartCat25@users.noreply.github.com>
2022-08-11 18:00:46 +0300

    Fixed bugs.

--- 2d items/assets/minecraft/shaders/include/flat_item.glsl
+++ 2d items/assets/minecraft/shaders/include/flat_item.glsl
@@ -8,18 +8,18 @@ vertexColor = minecraft_mix_light(Light0_Direction, Light1_Direction, Normal, Co
 
 if (FogStart < 100000 && texSize.x > 256 && abs(length(Normal) - 0.625) < 0.01)
 {
-    if (abs(abs(Normal.z) - 0.625) > 0.01)
-    {
-        vec3 offset = vec3(0, corner.y, 0) * IViewRotMat + vec3(corner.x, 0, 0) * (gl_VertexID / 4 % 2 * 2 - 1);
-        gl_Position = ProjMat * ModelViewMat * vec4(Position + offset * 0.25, 1.0);
+    vec3 absNormal = IViewRotMat * normalize(Normal);
 
-        vertexColor = texelFetch(Sampler2, UV2 / 16, 0) * Color;
+    if (abs(abs(absNormal.y) - 1) < 0.001)
+    {
+        vec3 offset = vec3(0, corner.y, 0) * IViewRotMat + vec3(corner.x, 0, 0) * (float(absNormal.y >= -0.1) * 2 - 1);
+        gl_Position = ProjMat * ModelViewMat * vec4(Position + offset * 0.25, 1.0);        
     }
     else 
     {
-        vec3 offset = vec3(corner * vec2(gl_VertexID / 4 % 2 * 2 - 1, 1), 0);
+        vec3 offset = vec3(corner * vec2(float(Normal.y > 0) * 2 - 1, 1), 0);
         gl_Position = ProjMat * ModelViewMat * vec4(Position + offset * 0.25, 1.0);
-
-        vertexColor = texelFetch(Sampler2, UV2 / 16, 0) * Color;
     }
+
+    vertexColor = texelFetch(Sampler2, UV2 / 16, 0) * Color;
 }
