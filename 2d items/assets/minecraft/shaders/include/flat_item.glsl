7e8fdde5ba6128746bea8ab2b18300236716d7db
DartCat25 <82237282+DartCat25@users.noreply.github.com>
2022-09-12 09:57:01 +0300

    2d items: optifine fix

--- 2d items/assets/minecraft/shaders/include/flat_item.glsl
+++ 2d items/assets/minecraft/shaders/include/flat_item.glsl
@@ -1,13 +1,11 @@
 const vec2[4] corners = vec2[4](vec2(-1, 1), vec2(-1, -1), vec2(1, -1), vec2(1, 1));
 vec2 corner = corners[gl_VertexID % 4];
 
-vec2 texSize = textureSize(Sampler0, 0);
-
 gl_Position = ProjMat * ModelViewMat * vec4(Position, 1.0);
 #ifndef NO_LIGHTMAP
 vertexColor = minecraft_mix_light(Light0_Direction, Light1_Direction, Normal, Color) * texelFetch(Sampler2, UV2 / 16, 0);
 #endif
-if (FogStart < 100000 && texSize.x > 256 && abs(length(Normal) - 0.625) < 0.01) //Not in hand, atlas is not a mob, weird Normal length from display.
+if (FogStart < 100000 && abs(length(Normal) - 0.625) < 0.01) //Not in hand or GUI, weird Normal length from display.
 {
     vec3 absNormal = IViewRotMat * normalize(Normal);
 
